2532654f2162dd09c43a54e265fcffca5258ad1e
aortegadev18 <aortega071820@gmail.com>
2026-08-07 19:15:05 -0500

    Creacion base de repositorio

---
+++ main.py
@@ -0,0 +1,8 @@
+tareas = []
+
+def mostrar_menu():
+    print("\nGESTOR DE TAREAS")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Mostrar progreso")
+    print("4. Salir")
